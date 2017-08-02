2ec5e28eb881a5e98f47d6e3d9d0512eb75cca9c
Matthew Darling <matthew.darling@kirasystems.com>
2017-08-02 12:27:13 -0300

    Manually combine profiles, instead of using composite

--- example/apps/app-a/project.clj
+++ example/apps/app-a/project.clj
@@ -15,5 +15,7 @@
   {:shared {:source-paths [~(System/getenv "CONFIG_JAR_PATH")]}
    :dev [:shared
          {:dependencies [[org.clojure/tools.nrepl "0.2.12"]]}]
-   :uberjar [:shared
-             {:dependencies [[metrics-clojure "2.9.0"]]}]})
+   :uberjar {;;; Actual :uberjar profile contents
+             :dependencies [[metrics-clojure "2.9.0"]]
+             ;;; Copied from :shared profile
+             :source-paths [~(System/getenv "CONFIG_JAR_PATH")]}})
