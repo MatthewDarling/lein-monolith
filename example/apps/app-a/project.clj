f08b2dd2df8f6e2079f542b172ca19114cff2522
Matthew Darling <matthew.darling@kirasystems.com>
2017-08-02 14:40:49 -0300

    Add example plugin to app-a subproject

--- example/apps/app-a/project.clj
+++ example/apps/app-a/project.clj
@@ -7,4 +7,6 @@
   [[commons-io "2.5"]
    [example/lib-a "1.0.0"]
    [example/lib-b "0.2.0"]
-   [org.clojure/clojure "1.8.0"]])
+   [org.clojure/clojure "1.8.0"]]
+
+  :hooks [leiningen.exampleplug])
