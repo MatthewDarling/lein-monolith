ad6cf9d9524ec8f3ed6d0a7a89e58dfc846dd702
Matthew Darling <matthew.darling@kirasystems.com>
2017-08-02 14:40:02 -0300

    Add example plugin to example monolith

---
+++ example/tasks/exampleplug.clj
@@ -0,0 +1,24 @@
+(ns leiningen.exampleplug
+  (:require [leiningen.install :as install]
+            [leiningen.core.main :as lmain]))
+
+(defn- once
+  [project]
+  (lmain/info  "Triggering exampleplug"))
+
+(defn- abort [s]
+  (lmain/info s)
+  (lmain/abort))
+
+(defn exampleplug
+  {}
+  ([project]
+   (once project)))
+
+(defn exampleplug-hook
+  [task & args]
+  (once (first args))
+  (apply task args))
+
+(defn activate []
+  (hooke/add-hook #'install/install #'exampleplug-hook))
