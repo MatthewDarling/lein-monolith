b34c1a8ec71bb62789aad4446eac4bc604e3ea84
Matthew Darling <matthew.darling@kirasystems.com>
2017-08-02 11:57:11 -0300

    Minimum reproduction of composite profiles bug

--- example/apps/app-a/project.clj
+++ example/apps/app-a/project.clj
@@ -7,4 +7,11 @@
   [[commons-io "2.5"]
    [example/lib-a "1.0.0"]
    [example/lib-b "0.2.0"]
-   [org.clojure/clojure "1.8.0"]])
+   [org.clojure/clojure "1.8.0"]]
+
+  :profiles
+  {:shared {:source-paths [~(System/getenv "CONFIG_JAR_PATH")]}
+   :dev [:shared
+         {:dependencies [[org.clojure/tools.nrepl "0.2.12"]]}]
+   :uberjar [:shared
+             {:dependencies [[metrics-clojure "2.9.0"]]}]})
